25ca8a77ece90edc5ebf01795ac4aa50bccfc5a8
Claudio Bianco <claudiombianco@gmail.com>
2025-06-12 22:11:57 -0300

    js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejs-web
 nodejs-web
+
+
+curl -X POST https://nodejs-web-788bf99653c6.herokuapp.com/api/echo \
+  -H "Content-Type: application/json" \
+  -d '{"text": "Hello from curl!"}'
--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,14 @@ async function sendEcho(text) {
   const data = await res.json();
   console.log('Echoed:', data.echoed);
 }
+
+document.getElementById('sendBtn').addEventListener('click', async () => {
+  const input = document.getElementById('textInput').value;
+  const res = await fetch('/api/echo', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text: input })
+  });
+  const data = await res.json();
+  document.getElementById('echoResult').innerText = `Response: ${data.echoed}`;
+});
